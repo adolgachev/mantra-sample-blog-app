74bbcdeb7fadfbc5d88bc9aa93e876f836689ea3
Andrey Dolgachev <andrey@andreyd.com>
2016-01-27 16:00:50 +1300

    Begin applying Material UI

--- client/components/layouts.main/index.jsx
+++ client/components/layouts.main/index.jsx
@@ -1,21 +1,29 @@
-import Navigations from '../navigations/index.jsx';
 import React from 'react';
 
+import AppBar from 'material-ui/lib/app-bar';
+import AppCanvas from 'material-ui/lib/app-canvas';
+import Paper from 'material-ui/lib/paper';
+
+import Navigations from '../navigations/index.jsx';
+
 const Layout = ({content = () => null }) => (
-  <div>
-    <header>
-    <h1>Mantra Voice</h1>
-    <Navigations />
-    </header>
+  <AppCanvas>
+    <div className="container-fluid">
 
-    <div>
-    {content()}
+      <AppBar title="Mantra Sample" />
+
+      <div className="row middle-xs">
+        <Navigations />
+      </div>
+
+      <div className="row middle-xs">
+        <Paper className="col-sm-12">
+          <main className="box">{content()}</main>
+        </Paper>
+      </div>
+
     </div>
-
-    <footer>
-    <small>Built with <a href='https://github.com/kadirahq/mantra'>Mantra</a> & Meteor.</small>
-    </footer>
-  </div>
+  </AppCanvas>
 );
 
 export default Layout;
--- client/main.js
+++ client/main.js
@@ -2,6 +2,13 @@ import loadMethodStubs from './configs/method_stubs';
 import {initContext} from './configs/context';
 import {initRoutes} from './configs/routes.jsx';
 import actions from './actions';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 loadMethodStubs();
 const context = initContext();
